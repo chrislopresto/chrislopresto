c3e7168ad74b24fcecf986f2f42b0a148073a4ab
Chris LoPresto <chris@chrislopresto.com>
2021-06-07 00:12:32 -0400

    :recycle: Extract Signature component

---
+++ apps/website/components/signature/signature.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Image from 'next/image';
+
+export const Signature = () => {
+  return <Image src="/signature.svg" alt="Chris LoPresto's signature" width={118} height={40} />;
+};
