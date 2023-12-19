bc798d175f4af0ece65ca4501b9bf225aecfa006
Chris LoPresto <chris@chrislopresto.com>
2023-12-18 22:06:07 -0500

    :fire: Remove Next website prototype

--- apps/website/.storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: [
-    '../components/**/*.stories.mdx',
-    '../pages/**/*.stories.mdx',
-    '../components/**/*.stories.@(js|jsx|ts|tsx)',
-    '../pages/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-};
--- apps/website/scripts/storybook-clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -uxo pipefail
-
-rm -rf storybook-build
-rm -rf ./public/storybook
