783aa4cef8e0ffb7a6005d0d2d9e759d0e624dd3
Chris LoPresto <chris@chrislopresto.com>
2023-12-18 22:06:07 -0500

    :fire: Remove Next website prototype

--- apps/website/components/signature/signature.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Image from 'next/image';
-
-export const Signature = () => {
-  return <Image src="/signature.svg" alt="Chris LoPresto's signature" width={118} height={40} />;
-};
--- apps/website/scripts/storybook-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -uxo pipefail
-
-pnpm storybook:clean
-pnpm build-storybook -s public -o ./storybook-build
-cp -r storybook-build/. ./public/storybook
