c633e178b7279ae2a75806d4d62d5543f665b9aa
Chris LoPresto <chris@chrislopresto.com>
2024-01-03 10:38:38 -0500

    :sparkles: Add Reveal JS Slide Deck component

---
+++ apps/website/app/components/reveal-js-slide-deck/reveal-js-slide-deck.stories.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/namespace */
+import * as React from 'react';
+import { StoryObj } from '@storybook/react';
+import type { Meta } from '@storybook/react';
+
+import { RevealJsSlideDeck } from './reveal-js-slide-deck';
+
+type StoryComponent = React.ComponentProps<typeof RevealJsSlideDeck>;
+
+const meta: Meta<StoryComponent> = {
+  title: 'Components/Reveal JS Slide Deck',
+  parameters: {
+    layout: 'fullscreen',
+  },
+  argTypes: {
+    slug: {
+      options: ['living-style-guide-driven-development', 'betterment-rebranding-bonanza'],
+      control: 'select',
+    },
+    title: { type: 'string' },
+  },
+  args: {
+    slug: 'living-style-guide-driven-development',
+    title: 'Living Style Guide Driven Development',
+  },
+};
+export default meta;
+
+type Story = StoryObj<StoryComponent>;
+
+export const Example: Story = {
+  render: ({ ...args }) => <RevealJsSlideDeck {...args} />,
+};
+export const WithoutControls: Story = {
+  render: ({ ...args }) => <RevealJsSlideDeck showControls={false} {...args} />,
+};
+export const SpecificSize: Story = {
+  render: ({ ...args }) => <RevealJsSlideDeck {...args} />,
+  parameters: {
+    layout: 'padded',
+  },
+  argTypes: {
+    width: { type: 'string' },
+    height: { type: 'string' },
+  },
+  args: {
+    width: '300px',
+    height: '169px',
+  },
+};
