dbb27ac40d35d2f1b130a837c5555a32855db2bd
Chris LoPresto <chris@chrislopresto.com>
2024-01-07 14:19:34 -0500

    :sparkles: Add appearance controls

--- apps/website/app/root.tsx
+++ apps/website/app/root.tsx
@@ -1,14 +1,13 @@
 import type { LinksFunction } from '@remix-run/cloudflare';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
-import { useAtomValue } from 'jotai';
 import styles from './index.css';
-import { colorSchemeAtom, useColorScheme } from './styles/color-scheme';
+import { useColorScheme } from './styles/color-scheme';
 import { css } from '../styled-system/css';
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export default function App() {
-  const [colorScheme] = useColorScheme();
+  const { colorScheme } = useColorScheme();
   return (
     <html lang="en">
       <head>
