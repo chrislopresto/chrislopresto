e8fa6b99e8778820b6cedca46d8d7000d2ee018e
Chris LoPresto <chris@chrislopresto.com>
2025-01-04 00:36:28 -0500

    :memo: Configure Storybook theme

---
+++ apps/website/.storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Chris LoPresto',
+  brandUrl: 'https://chrislopresto.com',
+  brandImage: 'https://get.chrislopresto.com/chris-lopresto-signature.png',
+  brandTarget: '_self',
+});
