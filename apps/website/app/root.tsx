b9e098a37bd456642376e1e4ee90a5bf6e26d537
Chris LoPresto <chris@chrislopresto.com>
2025-01-08 22:54:34 -0500

    :lipstick: Respect preferred color scheme

--- apps/website/app/root.tsx
+++ apps/website/app/root.tsx
@@ -1,4 +1,4 @@
-import { useColorScheme } from './styles/color-scheme';
+import { COLOR_SCHEME_KEY, useColorScheme } from './styles/color-scheme';
 import { css } from '../styled-system/css';
 
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router';
@@ -14,17 +14,28 @@ export const links: Route.LinksFunction = () => [{ rel: 'stylesheet', href: styl
 function App() {
   const { colorScheme } = useColorScheme();
   return (
-    <html lang="en">
+    <html lang="en" data-color-mode={colorScheme}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (typeof window !== 'undefined') {
+                const colorSchemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+                const preferredColorScheme = colorSchemeMediaQuery.matches ? 'dark' : 'light';
+                const storedColorScheme = localStorage.getItem('${COLOR_SCHEME_KEY}')?.replace(/"/g, '');
+                const resolvedColorScheme = storedColorScheme || preferredColorScheme;
+
+                document.documentElement.setAttribute('data-color-mode', resolvedColorScheme);
+              }
+            `,
+          }}
+        />
       </head>
-      <body
-        data-color-mode={colorScheme}
-        className={css({ p: '2', minWidth: '320px', maxWidth: '1000px', textStyle: 'body', bg: 'background' })}
-      >
+      <body className={css({ p: '2', minWidth: '320px', maxWidth: '1000px', textStyle: 'body', bg: 'background' })}>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
